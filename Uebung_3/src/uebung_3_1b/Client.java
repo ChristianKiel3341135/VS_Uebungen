455e61f0bcb988454859b92e78959e7e3c984cd7
Christian Kiel <christian.kiel@st.oth-regensburg.de>
2024-11-03 22:30:16 +0100

    aufgabe 1b fertig

--- Uebung_3/src/uebung_3_1b/Client.java
+++ Uebung_3/src/uebung_3_1b/Client.java
@@ -7,22 +7,29 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        try {
-            DatagramSocket socket = new DatagramSocket();
-            InetAddress ip = InetAddress.getLocalHost();
-            String input = sc.nextLine();
-            byte[] buffer = input.getBytes();
-            if(buffer.length <= 100){
-                socket.send(new DatagramPacket(buffer, buffer.length, ip, 8888));
+        while(true) {
+            try {
+                DatagramSocket socket = new DatagramSocket();
+                InetAddress ip = InetAddress.getLocalHost();
+                String input = sc.nextLine();
+                byte[] buffer = input.getBytes();
+                if(buffer.length <= 100){
+                    socket.send(new DatagramPacket(buffer, buffer.length, ip, 8888));
+                }
+                else System.out.println("Nachricht zu groß. Nachricht darf max. 100 Byte groß sein");
+
+                if(input.equals("Ende")){
+                    break;
+                }
+            } catch (UnknownHostException | SocketException e) {
+                e.printStackTrace();
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            else System.out.println("Nachricht zu groß. Nachricht darf max. 100 Byte groß sein");
 
-        } catch (UnknownHostException | SocketException e) {
-            e.printStackTrace();
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
 
 
     }
--- Uebung_3/src/uebung_3_1b/Server.java
+++ Uebung_3/src/uebung_3_1b/Server.java
@@ -1,4 +1,51 @@
 package uebung_3_1b;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class Server {
+    public static void main(String[] args) {
+        try {
+            DatagramSocket socket = new DatagramSocket(8888);
+            byte[] buffer = new byte[100];
+            while (true) {
+                DatagramPacket DpReceive = new DatagramPacket(buffer, buffer.length);
+
+                // Step 3 : revieve the data in byte buffer.
+                socket.receive(DpReceive);
+
+                System.out.println("Client:-" + ConvertByteToString(buffer));
+
+                // Exit the server if the client sends "bye"
+                if (ConvertByteToString(buffer).toString().equals("Ende"))
+                {
+                    System.out.println("Client hat Ende gesendet... Server wird gestoppt");
+                    break;
+                }
+
+                // Clear the buffer after every message.
+                buffer = new byte[65535];
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static StringBuilder ConvertByteToString(byte[] a)
+    {
+        if (a == null)
+            return null;
+        StringBuilder ret = new StringBuilder();
+        int i = 0;
+        while (a[i] != 0)
+        {
+            ret.append((char) a[i]);
+            i++;
+        }
+        return ret;
+    }
 }
